afad049e77c4c5f47b247fdf352094f7dfc89d54
Ishita05A <ishita05agarwal@gmail.com>
2025-05-27 16:39:59 +0530

    updated code

--- Day01/task2.java
+++ Day01/task2.java
@@ -3,10 +3,13 @@ import java.util.Scanner;
 public class task2 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+        System.out.println("Enter 1st value");
         int n=sc.nextInt();
+        System.out.println("Enter 2nd value");
         int m=sc.nextInt();
         if(n<m) System.out.println("lesser");
         else if(n==m) System.out.println("Equal");
         else System.out.println("lesser");
+        sc.close();
     }
 }
