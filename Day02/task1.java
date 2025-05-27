f4c759306b156f736c77fc1e3d917ddf3d31ef2d
Ishita05A <ishita05agarwal@gmail.com>
2025-05-27 16:28:23 +0530

    updated code

---
+++ Day02/task1.java
@@ -0,0 +1,14 @@
+package Day02;
+
+import java.util.Scanner;
+
+public class task1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter Number");
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+            System.out.println(i);
+        }
+    }
+}
