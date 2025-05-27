a66edaf41a563d77eb8fb0c4178c98b50a5e213b
Ishita05A <ishita05agarwal@gmail.com>
2025-05-27 16:38:28 +0530

    updated code

--- Day01/task2.java
+++ Day01/task2.java
@@ -1,7 +1,5 @@
 package Day01;
-
 import java.util.Scanner;
-
 public class task2 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
--- Day02/task2.java
+++ Day02/task2.java
@@ -1,15 +1,13 @@
 package Day02;
-
 import java.util.Scanner;
-
-public class task1 {
+public class task2 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        System.out.println("Enter Number");
+        System.out.println("Enter a Number");
         int n=sc.nextInt();
-        for(int i=1;i<=n;i++){
-            System.out.println(i);
-        }
+        if(n%2==0) System.out.println("Even");
+        else System.out.println("odd");
         sc.close();
     }
+    
 }
