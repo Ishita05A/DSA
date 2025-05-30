9054a7d3a4ab37af163d7f5b4d9fef5ecba14d48
Ishita05A <ishita05agarwal@gmail.com>
2025-05-30 17:10:10 +0530

    updated code

--- Day05/task2.java
+++ Day05/task2.java
@@ -1,31 +1,27 @@
-// SUM OF AN ARRAY
+// SUM OF ELEMENTS OF AN ARRAY
 package Day05;
 
 import java.util.Scanner;
 
 public class task2 {
+    static int sum(int[] arr) {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        return sum;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter no. of elements ");
         int n = sc.nextInt();
         int[] arr1 = new int[n];
-        int[] arr2 = new int[n];
-        int[] sum = new int[n];
         System.out.println("Enter elements of 1st array ");
         for (int i = 0; i < n; i++) {
             arr1[i] = sc.nextInt();
         }
-        System.out.println("Enter elements of 2nd array ");
-        for (int i = 0; i < n; i++) {
-            arr2[i] = sc.nextInt();
-        }
-        System.out.println("Sum of 2 array is: ");
-        for (int i = 0; i < n; i++) {
-            sum[i] = arr1[i] + arr2[i];
-        }
-        for (int i = 0; i < n; i++) {
-            System.out.print(sum[i] + " ");
-        }
+        System.out.println("Sum of elements of array = " + sum(arr1));
         sc.close();
     }
 }
