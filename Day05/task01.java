abfccd3f36cc3bbd6ab72b921f735529431e52cf
Ishita05A <ishita05agarwal@gmail.com>
2025-05-30 16:32:06 +0530

    updated code

---
+++ Day05/task01.java
@@ -0,0 +1,23 @@
+package Day05;
+
+import java.util.Scanner;
+
+public class task01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no. of elements");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter Elements");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Elements of array are : ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        sc.close();
+
+    }
+
+}
