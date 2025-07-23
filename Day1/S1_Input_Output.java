13d566fe79207718ed151ed09b7c8398a21b41f5
Ishita05A <ishita05agarwal@gmail.com>
2025-07-23 19:16:47 +0530

    updated code

---
+++ Day1/S1_Input_Output.java
@@ -0,0 +1,14 @@
+package Day1;
+
+import java.util.Scanner;
+
+public class S1_Input_Output {
+    static void input_output(Scanner sc) {
+        System.out.println(sc);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        input_output(sc);
+    }
+}
