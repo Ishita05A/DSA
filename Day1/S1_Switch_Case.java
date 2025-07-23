076f378753260eb540f129f4d0ff325890f62089
Ishita05A <ishita05agarwal@gmail.com>
2025-07-23 19:54:05 +0530

    updated code

---
+++ Day1/S1_Switch_Case.java
@@ -0,0 +1,40 @@
+package Day1;
+
+import java.util.Scanner;
+
+public class S1_Switch_Case {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter day number");
+        int n = sc.nextInt();
+        switch (n) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thrusday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Invalid");
+                break;
+        }
+        sc.close();
+
+    }
+
+}
