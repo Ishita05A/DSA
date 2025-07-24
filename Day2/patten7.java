b9b1f33fbbca496ae3c1026c10225a3e52344313
Ishita05A <ishita05agarwal@gmail.com>
2025-07-24 15:03:38 +0530

    updated code

---
+++ Day2/patten7.java
@@ -0,0 +1,27 @@
+package Day2;
+
+import java.util.Scanner;
+
+public class patten7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (j < i)
+                    System.out.print("  ");
+                else
+                    System.out.print("* ");
+            }
+            for (int j = 1; j < n; j++) {
+                if (j < n - i + 1)
+                    System.out.print("* ");
+
+            }
+            System.out.println();
+        }
+        sc.close();
+
+    }
+
+}
