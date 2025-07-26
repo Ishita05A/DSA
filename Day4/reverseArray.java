6b653d9db897895582f25e27d32252862edeb0f8
Ishita05A <ishita05agarwal@gmail.com>
2025-07-26 19:45:36 +0530

    updated code

---
+++ Day4/reverseArray.java
@@ -0,0 +1,38 @@
+package Day4;
+
+import java.util.Scanner;
+
+public class reverseArray {
+    static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    static void reverse_array(int[] arr) {
+        int n = arr.length;
+        int i = 0;
+        int j = n - 1;
+        while (i < j) {
+            swap(arr, i++, j--);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Eter no. of elements");
+        int n = sc.nextInt();
+        System.out.println("Enter elements");
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        reverse_array(arr);
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        sc.close();
+    }
+
+}
