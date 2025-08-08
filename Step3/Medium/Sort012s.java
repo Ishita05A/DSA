1917a2ec44aa1d64ec94c1392a870c8af8f73288
Ishita05A <ishita05agarwal@gmail.com>
2025-08-08 22:38:06 +0530

    updated code

---
+++ Step3/Medium/Sort012s.java
@@ -0,0 +1,49 @@
+package Step3.Medium;
+import java.util.*;
+public class Sort012s {
+    static void display(int[] arr){
+        for(int i = 0; i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    static void swap(int[] arr,int i,int j){
+        int temp = arr[i];
+        arr[i] =arr[j];
+        arr[j] = temp;
+    }
+    static void sort(int[] arr){
+        int low = 0;
+        int mid = 0;
+        int high = arr.length-1;
+        while(mid<=high){
+            if(arr[mid] == 0){
+                swap(arr, mid, low);
+                low++;
+            }
+            else if(arr[mid] == 1){
+                mid++;
+            }
+            else{
+                swap(arr, mid, high);
+                high--;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter no. of elements");
+        int n = sc.nextInt();
+        System.out.println("Enter elements");
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        sort(arr);
+        display(arr);
+        sc.close();
+
+
+    }
+    
+}
