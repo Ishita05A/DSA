82fc36a59023259ec8fa626c6b8e551feca53b10
Ishita05A <ishita05agarwal@gmail.com>
2025-08-14 00:34:49 +0530

    updated code

---
+++ Step6/LL_1D/LengthOfLL.java
@@ -0,0 +1,42 @@
+package Step6.LL_1D;
+
+public class LengthOfLL {
+    public static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    static void display(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+    static int findLength(Node head){
+        Node temp = head;
+        int count = 0;
+        while(temp!=null){
+            count++;
+            temp = temp.next;
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        Node a = new Node(5);
+        Node b = new Node(3);
+        Node c = new Node(4);
+        Node d = new Node(9);
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        display(a);
+        System.out.println(findLength(a));
+    }
+    
+}
