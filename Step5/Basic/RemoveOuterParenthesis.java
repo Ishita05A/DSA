9c351bb1c64f8e9630cc2823f2e34c9b5b878425
Ishita05A <ishita05agarwal@gmail.com>
2025-08-23 19:53:56 +0530

    updated code

---
+++ Step5/Basic/RemoveOuterParenthesis.java
@@ -0,0 +1,34 @@
+package Step5.Basic;
+
+import java.util.Scanner;
+
+public class RemoveOuterParenthesis {
+    static String removeOuter(String str) {
+        StringBuilder s = new StringBuilder("");
+int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            
+            char ch = str.charAt(i);
+            if (ch == '(') {
+                
+                if(count > 0) s.append(ch);
+                count++;
+            } else if (ch == ')') {
+                count--;
+                if (count != 0)
+                    s.append(ch);
+            }
+        }
+        return s.toString();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Brackets");
+        String str = sc.nextLine();
+        System.out.println(removeOuter(str));
+        sc.close();
+
+    }
+
+}
