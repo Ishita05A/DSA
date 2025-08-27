26f05a81aecfa3ecd6f6c2eca199d0b5c1cd9822
Ishita05A <ishita05agarwal@gmail.com>
2025-08-27 19:08:19 +0530

    updated code

---
+++ Step4/BinarySearch1DArray/UpperBound.java
@@ -0,0 +1,39 @@
+package Step4.BinarySearch1DArray;
+
+import java.util.Scanner;
+
+public class UpperBound {
+    static int upper_Bound(int[] arr,int target){
+        int n = arr.length;
+        int st = 0;
+        int end = n-1;
+        int idx = -1;
+        while (st <= end) {
+            int mid = st + (end - st)/2;
+            if(arr[mid] < target) st = mid +1;
+            else if(arr[mid] > target) end = mid -1;
+            else if(arr[mid] == target) {
+                idx = mid;
+                st = mid + 1;
+            }
+        }
+        return idx;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter rows");
+        int n = sc.nextInt();
+        System.out.println("Enter elements");
+        int[] arr = new int[n];
+        for(int i = 0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Enter target");
+        int x = sc.nextInt();
+        System.out.println(upper_Bound(arr, x));
+        sc.close();
+
+        
+    }
+    
+}
